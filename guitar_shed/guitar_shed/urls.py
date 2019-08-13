d9d61bb6555a6d39a0c53286e5550eb9d147bfbe
Ubuntu <ubuntu@ip-172-31-39-232.ec2.internal>
2019-08-13 10:23:46 +0000

    added cart and media paths

--- guitar_shed/guitar_shed/urls.py
+++ guitar_shed/guitar_shed/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
 from store import urls as urls_store
+from cart import urls as urls_cart
 from store.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -26,5 +27,6 @@ urlpatterns = [
     url(r'^$', all_products, name='index'),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^store/', include(urls_store)),
+    url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
