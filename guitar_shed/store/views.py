800d44a9889be482f148da84536581b171eed656
Ubuntu <ubuntu@ip-172-31-39-232.ec2.internal>
2019-08-13 10:25:51 +0000

    updated

--- guitar_shed/store/views.py
+++ guitar_shed/store/views.py
@@ -3,5 +3,5 @@ from .models import Product
 
 # Create your views here.
 def all_products(request):
-    store = Product.objects.all()
-    return render(request, "store.html", {"store": store})
+    products = Product.objects.all()
+    return render(request, "store.html", {"products": products})
