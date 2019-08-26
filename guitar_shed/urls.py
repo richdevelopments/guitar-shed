67e3a8c73ded9049c5c4f4083f7fa2128d9c45ae
Ubuntu <ubuntu@ip-172-31-39-232.ec2.internal>
2019-08-26 21:35:12 +0000

    added imports polls and threads

--- guitar_shed/urls.py
+++ guitar_shed/urls.py
@@ -23,6 +23,9 @@ from checkout import urls as urls_checkout
 from store.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
+from threads import views as forum_views
+from polls import api_views
+from threads import api_views as thread_api_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -32,5 +35,31 @@ urlpatterns = [
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
-    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
+        # Forum URLs
+    url(r'^forum/$', forum_views.forum),
+    url(r'^threads/(?P<subject_id>\d+)/$',
+        forum_views.threads, name='threads'),
+    url(r'^new_thread/(?P<subject_id>\d+)/$',
+        forum_views.new_thread, name='new_thread'),
+    url(r'^thread/(?P<thread_id>\d+)/$', forum_views.thread, name='thread'),
+    url(r'^post/new/(?P<thread_id>\d+)/$',
+        forum_views.new_post, name='new_post'),
+    url(r'^post/edit/(?P<thread_id>\d+)/(?P<post_id>\d+)/$',
+        forum_views.edit_post, name='edit_post'),
+    url(r'^post/delete/(?P<thread_id>\d+)/(?P<post_id>\d+)/$',
+        forum_views.delete_post, name='delete_post'),
+    url(r'^thread/vote/(?P<thread_id>\d+)/(?P<subject_id>\d+)/$',
+        forum_views.thread_vote, name='cast_vote'),
+        # REST URLs
+    url(r'^threads/polls/$', api_views.PollViewSet.as_view()),
+    url(r'^threads/polls/(?P<pk>[\d]+)$', api_views.PollInstanceView.as_view(),
+        name='poll-instance'),
+    url(r'^threads/polls/vote/(?P<thread_id>\d+)/$',
+        api_views.VoteCreateView.as_view(), name='create_vote'),
+    url(r'^post/update/(?P<pk>[\d+]+)/$',
+        thread_api_views.PostUpdateView.as_view(), name="update-poll"),
+    url(r'post/delete/(?P<pk>[\d]+)/$',
+        thread_api_views.PostDeleteView.as_view(), name='delete-poll')
 ]
+
